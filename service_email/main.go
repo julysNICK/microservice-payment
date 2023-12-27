f5256a661e633d35e888e319bdf204ebaa2d5795
julysNICK <julystube@gmail.com>
2023-12-27 14:07:57 -0300

    add grpc config

--- service_email/main.go
+++ service_email/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"service_email/gapi"
 	"service_email/pb"
 	"service_email/util/config"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func runGrpcServer(config config.Config) {
+
+	fmt.Println("starting grpc server...")
 
 	server, err := gapi.NewServer(config)
 
@@ -21,6 +25,25 @@ func runGrpcServer(config config.Config) {
 
 	pb.RegisterEmailServiceServer(grpcServer, server)
 
+	fmt.Println("starting grpc server... line 28")
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	fmt.Println("starting grpc server... line 32")
+	if err != nil {
+		fmt.Println("failed to listen: ", err)
+	}
+
+	fmt.Println("starting grpc server... line 37")
+
+	errServer := grpcServer.Serve(listener)
+	fmt.Println("starting grpc server... line 40")
+	if errServer != nil {
+		fmt.Println("failed to serve: ", err)
+	}
+
+	fmt.Println("starting grpc server... line 45")
+
 	fmt.Println("starting grpc server...")
 }
 
@@ -28,8 +51,9 @@ func main() {
 	config, err := config.LoadConfig(".")
 
 	if err != nil {
-		fmt.Errorf("failed to load config: %w", err)
+		fmt.Println("failed to load config: ", err)
 	}
 
 	runGrpcServer(*config)
+	fmt.Println("starting grpc server...")
 }
